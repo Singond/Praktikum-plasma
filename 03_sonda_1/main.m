2d5d53ffb933f55b3ed75c83da528fc5e33b95eb
Jan Singon Slany <singond@seznam.cz>
2022-04-08 00:08:42 +0200

    03: Extract V-A characteristics

--- 03_sonda_1/main.m
+++ 03_sonda_1/main.m
@@ -1,5 +1,8 @@
 0;  # Not a function
 
+Id = [50 40 30 50 50 50 50];  ## Discharge current [mA]
+p = [50 50 50 20 10 5 100];   ## Pressure [Pa]
+
 function D = load_data(file)
 	filelocal = false;
 	if (is_valid_file_id(file))
@@ -42,13 +45,61 @@ function D = load_data(file)
 	endif
 endfunction
 
-function x = align(udata, idata)
-	x.U = udata.val;
-	x.I = idata.val;
-	starttime = min(udata.timelinearized(1), udata.timelinearized(1));
-	x.tU = udata.timelinearized - starttime;
-	x.tI = idata.timelinearized - starttime;
-end
+function [utime, itime] = align(udata, idata)
+	starttime = min(udata.timelinearized(1), idata.timelinearized(1));
+	utime = udata.timelinearized - starttime;
+	itime = idata.timelinearized - starttime;
+endfunction
+
+function [uu, ii, tt] = extract_vac(utime, uvals, itime, ivals, tranges, pts)
+	assert(columns(tranges), 2);
+	uranges = interp1(utime, uvals, tranges);
+	umin = max(min(uranges, [], 2));
+	umax = min(max(uranges, [], 2));
+	uu = linspace(umin, umax, pts)';
+
+	ii = NA(pts, rows(tranges));
+	for k = 1:rows(tranges)
+		tr = tranges(k,:);
+		tmin = min(tr);
+		tmax = max(tr);
+		sel = utime >= tmin & utime <= tmax;
+		## The voltage values have steps (they are not monotonic).
+		## Fit a parabola through them to correct this.
+		ts = utime(sel);
+		us = uvals(sel);
+		bb = ols(us, [ts.**2 ts ones(size(ts))]);
+		a = bb(1);
+		b = bb(2);
+		c = bb(3);
+		## Calculate time values corresponding to uu.
+		## Use trial and error to select the correct root
+		## of the quadratic equation.
+		t0 = (-b + sqrt(b**2 - 4*a*(c - mean([umin umax])))) / (2*a);
+		if (t0 > tmin && t0 < tmax)
+			tk = (-b + sqrt(b.**2 - 4*a.*(c - uu))) ./ (2*a);
+		else
+			tk = (-b - sqrt(b.**2 - 4*a.*(c - uu))) ./ (2*a);
+		endif
+		ii(:,k) = interp1(itime, ivals, tk);
+		tt(:,k) = tk;
+		k++;
+	endfor
+endfunction
+
+function x = extract_vacx(x, varargin)
+	[x.U, x.I] = extract_vac(x.Utime, x.Uraw, x.Itime, x.Iraw, varargin{:});
+endfunction
+
+function x = load_experiment(udata, idata)
+	x.Utimeerror = udata.timeerror;
+	x.Itimeerror = idata.timeerror;
+	x.Uunit = udata.unit;
+	x.Iunit = idata.unit;
+	x.Uraw = udata.val;
+	x.Iraw = -idata.val;
+	[x.Utime, x.Itime] = align(udata, idata);
+endfunction
 
 U = struct();
 I = struct();
@@ -56,5 +107,19 @@ X = struct();
 for k = 1:7
 	U(k) = load_data(sprintf("data/voltage%02d.txt", k));
 	I(k) = load_data(sprintf("data/current%02d.txt", k));
-	X(k) = align(U(k), I(k));
+	x = load_experiment(U(k), I(k));
+	x.Id = Id(k);
+	x.p = p(k);
+	X(k) = x;
 endfor
+clear U I k;
+
+X(1).U = [];
+X(1).I = [];
+X(1) = extract_vacx(X(1), [7 277; 288 555], 500);
+X(2) = extract_vacx(X(2), [15 282; 288 557], 500);
+X(3) = extract_vacx(X(3), [12 281; 288 558], 500);
+X(4) = extract_vacx(X(4), [13 281; 291 560], 500);
+X(5) = extract_vacx(X(5), [10 281; 315 583], 500);
+X(6) = extract_vacx(X(6), [12 282; 288 556], 500);
+X(7) = extract_vacx(X(7), [291 559], 500);
--- 03_sonda_1/show.m
+++ 03_sonda_1/show.m
@@ -1,14 +1,40 @@
 main;
 
-function plot_experiment(x)
-	ax = plotyy(x.tU, x.U, x.tI, x.I);
+function plot_rawdata(x)
+	ax = plotyy(x.Utime, x.Uraw, x.Itime, x.Iraw);
 	xlabel("time t [s]");
 	ylabel(ax(1), "voltage U [V]");
 	ylabel(ax(2), "current I [uA]");
 endfunction
 
-close all;
-for x = X
-	figure();
-	plot_experiment(x);
-endfor
+function plot_rawdata_all(X)
+	for x = X
+		figure();
+		plot_rawdata(x);
+	endfor
+endfunction
+
+function plot_vac(X)
+	clf;
+	ax = gca();
+	hold on;
+	for x = X
+		color = get(ax, "colororder")(get(ax, "colororderindex"), :);
+		first = true;
+		for ii = x.I
+			args = {};
+			if (first)
+				args(end+[1 2]) = {"displayname",...
+					sprintf("%.0f mA, %.0f Pa", x.Id, x.p)};
+			else
+				args(end+[1 2]) = {"handlevisibility", "off"};
+			endif
+			plot(ax, x.U, ii, "color", color, args{:});
+			first = false;
+		endfor
+	endfor
+	hold off;
+	xlabel(ax, "voltage U [V]");
+	ylabel(ax, "current I [uA]");
+	legend location northwest;
+endfunction
