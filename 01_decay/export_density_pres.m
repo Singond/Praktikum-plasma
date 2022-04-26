610febb32a3031e6325d4a4173b77cf7f85fdfef
Jan Singon Slany <singond@seznam.cz>
2022-04-27 00:23:36 +0200

    01: Split export script into one script per plot

---
+++ 01_decay/export_density_pres.m
@@ -0,0 +1,14 @@
+pkg load report;
+
+main;
+
+nscale = 1e15;
+gp = gnuplotter;
+gp.load("../plotsettings.gp");
+gp.plot(p, n0./nscale, "w p");
+gp.exec("\
+	set xlabel 'tlak $\\pres\\,[\\si{\\pascal}]$' \n\
+	set ylabel '$\\dens\\,[\\SI{e-15}{\\metre^{-3}}]$' \n\
+	set key off \n\
+");
+gp.export("plots/density-pres.tex", "epslatex", "size 8cm,6cm");
---
+++ 01_decay/export_diffuse_pres.m
@@ -0,0 +1,13 @@
+pkg load report;
+
+main;
+
+gp = gnuplotter;
+gp.load("../plotsettings.gp");
+gp.plot(p, D, "w p");
+gp.exec("\
+	set xlabel 'tlak $\\pres\\,[\\si{\\pascal}]$' \n\
+	set ylabel '$\\diffuse\\,[\\si{\\diffuseunit}]$' \n\
+	set key off \n\
+");
+gp.export("plots/diffuse-pres.tex", "epslatex", "size 8cm,6cm");
