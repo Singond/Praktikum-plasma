0d8a37dc6c51a4c120631a183691f8578f9ef336
Jan Singon Slany <singond@seznam.cz>
2022-05-10 02:07:57 +0200

    01: Add plots of transformed results

---
+++ 01_decay/export_density_inv.m
@@ -0,0 +1,19 @@
+pkg load report;
+
+main;
+
+gp = gnuplotter;
+gp.load("../plotsettings.gp");
+nscale = 1e-14;
+for k = 1:numel(X)
+	x = X(k);
+	gp.plot(x.tr, (1./x.n)./nscale,
+		sprintf("w p ls %d t '\\SI{%g}{\\pascal}'", k, x.p));
+	tt = linspace(min(x.tr), max(x.tr), 1000);
+	gp.plot(tt, 1./densitymodel(tt, x.nlfit.DoL, x.nlfit.a, x.nlfit.c)./nscale,
+		sprintf("w l ls %d t ''", k));
+endfor
+gp.xlabel('čas $\\tm\\,[\\si{\\micro\\second}]$');
+gp.exec("set key top right samplen 2 spacing 1.5 at graph 0.20,0.9");
+gp.ylabel('$\\num{e-14}/(\\dens/\\si{\\metre^{3}})$');
+gp.export("plots/density-inv.tex", "epslatex", "size 17cm,11cm");
