6833c7a20c7caead2cb497439762f063e3ff0055
Jan Slany <jan.slany@ais-servis.cz>
2022-05-10 15:45:23 +0200

    01: Edit plot label and description

--- 01_decay/export_density_pres.m
+++ 01_decay/export_density_pres.m
@@ -8,7 +8,7 @@ gp.load("../plotsettings.gp");
 gp.plot(p, n0./nscale, "w p");
 gp.exec("\
 	set xlabel 'tlak $\\pres\\,[\\si{\\pascal}]$' \n\
-	set ylabel '$\\dens\\,[\\SI{e-15}{\\metre^{-3}}]$' \n\
+	set ylabel '$\\densinit\\,[\\SI{e-15}{\\metre^{-3}}]$' \n\
 	set key off \n\
 ");
 gp.export("plots/density-pres.tex", "epslatex", "size 8cm,6cm");
