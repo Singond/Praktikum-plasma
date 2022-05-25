7114417a860eec0a7c6d3c754b664f8f595a1b1d
Jan Singon Slany <singond@seznam.cz>
2022-05-25 22:03:17 +0200

    01: Show uncertainties in table

--- 01_decay/export_results.m
+++ 01_decay/export_results.m
@@ -14,5 +14,29 @@ fputs(f, hdr);
 fputs(f, "\n");
 invfit = [[[X.invfit].a]' [[X.invfit].n]'];
 logfit = [[[X.logfit].DoL]'./L^2 [[X.logfit].n]'];
-dlmwrite(f, [p [X.I]' logfit invfit D a n0], "\t");
+num = "%f";
+unc = "%f+-%f";
+nscale = 1e15;
+ascale = 1e-20;
+for x = X
+	fprintf(f, "%.0f", x.p);
+	fwrite(f, "\t");
+	fprintf(f, num, x.I);
+	fwrite(f, "\t");
+	fprintf(f, unc, x.logfit.DoL./L^2, x.logfit.DoL_ste./L^2);
+	fwrite(f, "\t");
+	fprintf(f, unc, x.logfit.n/nscale, x.logfit.n_ste/nscale);
+	fwrite(f, "\t");
+	fprintf(f, unc, x.invfit.a/ascale, x.invfit.a_ste/ascale);
+	fwrite(f, "\t");
+	fprintf(f, unc, x.invfit.n/nscale, x.invfit.n_ste/nscale);
+	fwrite(f, "\t");
+	fprintf(f, unc, x.D, x.nlfit.D_ste);
+	fwrite(f, "\t");
+	fprintf(f, unc, x.a/ascale, x.nlfit.a_ste/ascale);
+	fwrite(f, "\t");
+	fprintf(f, unc, x.n0/nscale, x.n0_ste/nscale);
+	fwrite(f, "\n");
+endfor
 fclose(f);
+
