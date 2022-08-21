b64ee6cb500f7784d4cc604c87f96a93504c4f0f
Jan Singon Slany <singond@seznam.cz>
2022-08-21 14:17:55 +0200

    03: Edit helper plot function

--- 03_probes/octave/plot_rawdata.m
+++ 03_probes/octave/plot_rawdata.m
@@ -1,6 +1,10 @@
-function plot_rawdata(x)
-	ax = plotyy(x.Utime, x.Uraw, x.Itime, x.Iraw);
+function plot_rawdata(x, y)
+	if (nargin == 1)
+		ax = plotyy(x.Utime, x.Uraw, x.Itime, x.Iraw);
+		ylabel(ax(1), "voltage U [V]");
+		ylabel(ax(2), "current I [uA]");
+	elseif (nargin == 2)
+		ax = plotyy(x.timelinearized, x.val, y.timelinearized, y.val);
+	endif
 	xlabel("time t [s]");
-	ylabel(ax(1), "voltage U [V]");
-	ylabel(ax(2), "current I [uA]");
 endfunction
