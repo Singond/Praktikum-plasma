2e157b326df6d0ff1cdc41ce3b6cd9769f6df41c
Jan Singon Slany <singond@seznam.cz>
2022-08-21 22:22:24 +0200

    03: Enable alternative arguments in function

--- 03_probes/octave/plot_separated.m
+++ 03_probes/octave/plot_separated.m
@@ -5,8 +5,18 @@ function plot_separated(D, D1, D2, sep)
 	hold on;
 	set(gca, "colororderindex", color);
 	plot(D.timelinearized, sep, "--");
-	plot(D1.timelinearized, D1.val, "o");
-	plot(D2.timelinearized, D2.val, "x");
+
+	if (isfield(D1, "timelinearized"))
+		plot(D1.timelinearized, D1.val, "o");
+	elseif (isfield(D1, "Itime"))
+		plot(D1.Itime, -D1.Iraw, "o");
+	endif
+	if (isfield(D2, "timelinearized"))
+		plot(D2.timelinearized, D2.val, "x");
+	elseif (isfield(D2, "Itime"))
+		plot(D2.Itime, -D2.Iraw, "x");
+	endif
+
 	if (!washold)
 		hold off;
 	endif
