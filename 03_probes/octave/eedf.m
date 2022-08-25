97f926a2ff543c2e31c87ab2194f480bc532c32b
Jan Singon Slany <singond@seznam.cz>
2022-08-25 18:21:11 +0200

    03: Split helper function

--- 03_probes/octave/eedf.m
+++ 03_probes/octave/eedf.m
@@ -1,5 +1,13 @@
+## -*- texinfo -*-
+## @deftypefn  {} {@var{f} =} eedf (@var{u}, @var{i})
+## @deftypefnx {} {@var{f} =} eedf (@dots{}, @var{a})
+##
+## Calculate electron energy distribution function from voltage @var{u}
+## and current @var{i}.
+##
+## Probe surface area can be given as the argument @var{a}.
+## The default value is 1.
+## @end deftypefn
 function f = eedf(u, i, A = 1)
-	global electronmass elemcharge
-	f = (1/A) .* sqrt(8 .* electronmass .* elemcharge.^-3 .* abs(u(1:end-2)))...
-		.* diff(i, 2) ./ diff(u(1:end-1)).^2;
+	f = eedfd(u(1:end-2), diff(i, 2) ./ diff(u(1:end-1)).^2, A);
 endfunction
---
+++ 03_probes/octave/eedfd.m
@@ -0,0 +1,16 @@
+## -*- texinfo -*-
+## @deftypefn  {} {@var{f} =} eedf (@var{u}, @var{didu2})
+## @deftypefnx {} {@var{f} =} eedf (@dots{}, @var{a})
+##
+## Calculate electron energy distribution function from voltage @var{u}
+## and @var{didu2}, which is the second derivative of current with respect
+## to voltage @var{u}.
+##
+## Probe surface area can be given as the argument @var{a}.
+## The default value is 1.
+## @end deftypefn
+function f = eedfd(u, didu2, A = 1)
+	global electronmass elemcharge
+	f = (1/A) .* sqrt(8 .* electronmass .* elemcharge.^-3 .* abs(u))...
+		.* didu2;
+endfunction
