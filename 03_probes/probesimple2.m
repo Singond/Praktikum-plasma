7a250f9928b01f4b48d9a32c53f3dc1d5757c48b
Jan Singon Slany <singond@seznam.cz>
2022-08-29 18:26:49 +0200

    03: Put separated current data into one struct

--- 03_probes/probesimple2.m
+++ 03_probes/probesimple2.m
@@ -11,21 +11,26 @@ global Ua = 1.0;                     # Alternating voltage added [V]
 Id = [50 50 30];                     # Discharge current [mA]
 p = [5 20 20];                       # Pressure [Pa]
 
-U = struct();
-I = struct();
-I1 = struct();
-I2 = struct();
-S = struct();
-X = struct();
-Y = struct();
+U = struct();                        # Raw voltage data
+I = struct();                        # Raw current data
+S = struct();                        # Data separation check
+X = struct();                        # Experiment without alternating voltage
+Y = struct();                        # Experiment with alternating voltage
 for k = 1:3
 	U(k) = load_data(sprintf("data/2022-04-05/voltage%02d.txt", k + 4));
 	I(k) = load_data(sprintf("data/2022-04-05/current%02d.txt", k + 4));
+
 	## Align times now before separating data
 	[U(k).timelinearized, I(k).timelinearized] = align(U(k), I(k));
-	[I1(k), I2(k), S(k).sep] = separate_data(I(k), 30, 0.5);
-	x = load_experiment(U(k), I1(k));
-	y = load_experiment(U(k), I2(k));
+
+	## Separate data and store individual parts for later inspection
+	S(k).all = I(k);
+	[i1, i2, S(k).sep] = separate_data(I(k), 30, 0.5);
+	S(k).upper = i1;
+	S(k).lower = i2;
+
+	x = load_experiment(U(k), i1);
+	y = load_experiment(U(k), i2);
 	x.Uraw = y.Uraw *= -1;    # Apparently, the voltmeter polarity was wrong
 	x.Id = y.Id = Id(k);
 	x.p = y.p = p(k);
