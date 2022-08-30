937ec2b1ef3cb158e896ce4284d99f37785b4211
Jan Singon Slany <singond@seznam.cz>
2022-08-30 17:11:37 +0200

    03: Fix updating plot colours in EEDF plot

--- 03_probes/octave/plot_eedf.m
+++ 03_probes/octave/plot_eedf.m
@@ -1,5 +1,12 @@
 function plot_eedf(x)
-	color = get(gca, "colororder")(get(gca, "colororderindex"), :);
+	if (ishold)
+		coloridx = get(gca, "colororderindex");
+	else
+		coloridx = 1;
+	endif
+	color = get(gca, "colororder")(coloridx, :);
+	## Use exactly one plot without color ("b") in third argument
+	## to ensure advancing to the next color in palette.
 	plot(
 		x.eedfa_E, x.eedfa, "", "color", color,
 		x.eedfa_E, x.mbfit.f(x.eedfa_E), "b--", "color", color,
