ddf86514c7fa5dd5c431651d93e8b33b11f017f1
Jan Singon Slany <singond@seznam.cz>
2022-09-05 23:49:20 +0200

    03: Plot intersection method steps

--- 03_probes/export_double.m
+++ 03_probes/export_double.m
@@ -6,15 +6,29 @@ k = 1;
 for x = D
 	gp = gnuplotter();
 	gp.load("../plotsettings.gp");
-	gp.plot(x.U, x.Im, 'w l ls 1 t "$\\iprobe$ (celkový)"');
-##	gp.plot(x.U, x.Ii, 'w l lc "black" dt 2 t "$\\iion$ (iontový)"');
-##	gp.plot(x.U, x.Ie, 'w l ls 2 dt 4 t "$\\ielec$ (elektronový)"');
-	gp.xlabel('potenciál sondy $\\potprobe\\,[\\si{\\volt}]$');
+	gp.plot(x.U, x.Im, 'w l ls 2 t "$\\iprobe$ (celkový)"');
+	gp.plot(x.U, x.fita.f(x.U), 'w l ls 2 dt 2 t "$\\ielec$ (iontový)"');
+	gp.plot(x.U, x.fitc.f(x.U), 'w l ls 2 dt 2 t ""');
+	y = x.fitb.f(x.U);
+	m = y >= min(x.Im) & y <= max(x.Im);
+	gp.plot(x.U(m), x.fitb.f(x.U(m)), 'w l ls 1 dt 3 t ""');
+	gp.plot([x.al; x.ar], [x.Ip2; x.Ip1], 'w p ls 2 pt 13 t ""');
+	gp.exec("\
+		set arrow nohead from first 0, graph 0 to first 0, graph 1 lc 'black' dt 2 \n\
+		set arrow nohead from graph 0, first 0 to graph 1, first 0 lc 'black' dt 2 \n\
+		");
+	gp.xlabel('napětí sondy $\\udoubleprobe\\,[\\si{\\volt}]$');
 	gp.ylabel('sondový proud $\\iprobe\\,[\\si{\\micro\\ampere}]$');
 	gp.exec("\
 		set xtics 4 \n\
-		set key top left reverse Left samplen 2 height 1 \n\
+		set key bottom right reverse Left samplen 2 height 1 \n\
+		unset key \n\
 	");
+	gp.exec(sprintf("set label \
+\"$\\\\idisch=\\\\SI{%g}{\\\\milli\\\\ampere}$\\n\
+$\\\\pres=\\\\SI{%.0f}{\\\\pascal}$\" \
+		at graph 0.1,0.9",
+		x.Id, x.p));
 	gp.export(sprintf("plots/double-vac-%d.tex", k),
 		"epslatex", "size 8cm,6cm");
 
