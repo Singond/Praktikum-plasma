268f5a0c8199d4d1afadd26bfc056787b1954cdd
Jan Singon Slany <singond@seznam.cz>
2022-09-05 18:42:42 +0200

    03: Fit double probe VAC with lines

--- 03_probes/octave/plasmaprops_double.m
+++ 03_probes/octave/plasmaprops_double.m
@@ -1,3 +1,26 @@
 function x = plasmaprops_double(x)
 	x.Im = mean(x.I, 2);                # Mean probe current
+
+	## Fit parts of the VAC with lines
+
+	N = numel(x.U);
+	z = find(x.Im > 0)(1);
+
+	## Left part
+	sel = (1:round(0.3*N))';
+	beta = ols(x.Im(sel), [x.U(sel) ones(size(sel))]);
+	x.fita.beta = beta;
+	x.fita.f = @(U) beta(1)*U + beta(2);
+
+	## Central part
+	sel = (round(z - 0.05*N):round(z + 0.05*N))';
+	beta = ols(x.Im(sel), [x.U(sel) ones(size(sel))]);
+	x.fitb.beta = beta;
+	x.fitb.f = @(U) beta(1)*U + beta(2);
+
+	## Right part
+	sel = (round(0.7*N):N)';
+	beta = ols(x.Im(sel), [x.U(sel) ones(size(sel))]);
+	x.fitc.beta = beta;
+	x.fitc.f = @(U) beta(1)*U + beta(2);
 endfunction
--- 03_probes/show_double.m
+++ 03_probes/show_double.m
@@ -5,6 +5,29 @@ endif
 fig = 1;
 figure(fig++);
 clf;
+hold on;
 title("Total probe current");
-plot_vac(D);
+for k = 1:numel(D)
+	d = D(k);
+
+	## Data
+	set(gca, "colororderindex", k);
+	plot(d.U, d.Im);
+
+	## Left fit
+	set(gca, "colororderindex", k);
+	plot(d.U, d.fita.f(d.U), "--");
+
+	## Center fit
+	y = d.fitb.f(d.U);
+	m = y > min(d.Im) & y < max(d.Im);
+	set(gca, "colororderindex", k);
+	plot(d.U(m), y(m), "-.");
+
+	## Right fit
+	set(gca, "colororderindex", k);
+	plot(d.U, d.fitc.f(d.U), ":");
+	k++;
+endfor
+hold off;
 
