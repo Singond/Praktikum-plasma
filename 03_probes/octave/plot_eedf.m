5f9a2e2dc9458c8c4232bc01a77435312117e709
Jan Singon Slany <singond@seznam.cz>
2022-09-08 18:53:11 +0200

    03: Handle missing fits in plot function gracefully

--- 03_probes/octave/plot_eedf.m
+++ 03_probes/octave/plot_eedf.m
@@ -5,12 +5,28 @@ function plot_eedf(x)
 		coloridx = 1;
 	endif
 	color = get(gca, "colororder")(coloridx, :);
+
 	## Use exactly one plot without color ("b") in third argument
 	## to ensure advancing to the next color in palette.
+
+	## Data
 	plot(x.E, x.f, "", "color", color);
+
+	## Maxwell-Boltzmann distribution fit
 	plot(x.E, x.mb.f(x.E), "b--", "color", color);
+	if (isfield(x, "mbnl") && isfield(x.mbnl, "f"))
+		plot(x.E, x.mbnl.f(x.E), "b--", "color", color);
+	endif
+
+	## Druyvesteyn distribution fit
 	plot(x.E, x.dr.f(x.E), "b:", "color", color);
-	if (isfield(x, "gen"))
+	if (isfield(x, "drnl") && isfield(x.drnl, "f"))
+		## Have non-linear Druyvesteyn fit: Do not print linearized one
+		plot(x.E, x.drnl.f(x.E), "b:", "color", color);
+	endif
+
+	## General distribution fit
+	if (isfield(x, "gen") && isfield(x.gen, "f"))
 		plot(x.E, x.gen.f(x.E), "b-.", "color", color);
 	endif
 endfunction
